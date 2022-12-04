51766ad6ac532d8786ee5a5c7d695043f8f76768
YuTong <1162796235@qq.com>
2022-12-05 00:05:24 +0800

    第3次提交

--- src/main/java/com/itheima/SpringbootssmpApplication.java
+++ src/main/java/com/itheima/SpringbootssmpApplication.java
@@ -5,9 +5,9 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cache.annotation.EnableCaching;
 import org.springframework.scheduling.annotation.EnableScheduling;
 
-@EnableCaching// 开启缓存功能
+@EnableCaching//开启缓存功能
 @SpringBootApplication
-@EnableScheduling// 开启定时任务开关
+@EnableScheduling//开启定时任务开关
 public class SpringbootssmpApplication {
 
     public static void main(String[] args) {
